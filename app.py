7ccae8cafe04545877dbf114aee4b7923fcfdc28
Suyog Kumar Pokhrel <suyogpokhrel13@gmail.com>
2026-02-25 23:29:57 +0545

    add login register

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/perform_registration')
+def perform_registration():
+    return "Registration successful!"
+
+app.run(debug=True)
